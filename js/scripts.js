a3c8b9ba03f7c20c4e2d99f4d11434943f5f84a1
Brian Sigilai <briansigiali@gmail.com>
2018-03-26 17:32:45 +0300

    Added sidebar navigation

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".btn").click(function () {
+        var email = $(".place").val();
+        alert(email + " " + "has been succesfully subscribed.");
+    });
+
+});
+
+
